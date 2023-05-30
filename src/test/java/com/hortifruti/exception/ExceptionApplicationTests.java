511895f5e24a4597b22e51a84750ab3bb56e121b
Clibanez <clibanezzcaldas@gmail.com>
2023-05-30 20:56:02 -0300

    Projeto inicial

---
+++ src/test/java/com/hortifruti/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hortifruti.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
