998701c7c47c2eb04215dacf1ba5c89a628879f6
Clibanez <clibanezzcaldas@gmail.com>
2023-06-01 10:52:31 -0300

    Exemplo 2

---
+++ src/main/java/com/hortifruti/exception/exception/TratamentoDeErro.java
@@ -0,0 +1,20 @@
+package com.hortifruti.exception.exception;
+
+import com.hortifruti.exception.dto.ErrosException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice //Servi para falar que essa classe, vai tratar os erros de exception.
+public class TratamentoDeErro {
+
+    @ExceptionHandler(NoSuchElementException.class) //Passamos o parametro que vai ser tratado
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build(); //Aqui retornamos o erro 404 notFound() que e mais especifico para esse erro.
+    }
+
+}
